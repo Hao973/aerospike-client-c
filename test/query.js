aerospike/aerospike-client-nodejs

wait for UDF registration to be completed on all cluster nodes

test/query.js
@@ -54,12 +54,13 @@ describe('client.query()', function () {
       client.createStringIndex(indexObj, indexCreationCallback)
 
       // Register the UDFs to be used in aggregation.
-
       var dir = __dirname
       var filename = dir + '/aggregate.lua'
       client.udfRegister(filename, function (err) {
-        expect(err).to.be.ok()
         expect(err.code).to.equal(status.AEROSPIKE_OK)
+        client.udfRegisterWait('aggregate.lua', 10, function (err) {
+          expect(err.code).to.equal(status.AEROSPIKE_OK)
+        })
       })
 
       // load objects - to be queried in test case.
@@ -94,6 +95,7 @@ describe('client.query()', function () {
   })
 
   after(function (done) {
+    client.udfRemove('aggregate.lua', function () {})
     client.close()
     client = null
     done()

Hao973/aerospike-client-c

test/query.js
@@ -52,11 +52,13 @@ describe('Aerospike.query()', function () {
       Aerospike.createStringIndex(indexObj, indexCreationCallback)
 
       // Register the UDFs to be used in aggregation.
-
       var dir = __dirname
       var filename = dir + '/aggregate.lua'
       Aerospike.udfRegister(filename, function (err) {
         expect(err).not.to.be.ok()
+        Aerospike.udfRegisterWait('aggregate.lua', 10, function (err) {
+          expect(err).not.to.be.ok()
+        })
       })
 
       // load objects - to be queried in test case.
@@ -90,6 +92,7 @@ describe('Aerospike.query()', function () {
   })
 
   after(function (done) {
+    Aerospike.udfRemove('aggregate.lua', function () {})
     Aerospike.close()
     done()
   })